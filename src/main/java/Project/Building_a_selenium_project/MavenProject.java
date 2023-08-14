aa1a89ba0dc77cc1c54f6b354f4eee025c8c77b1
nageshvilaskolhe <kolhenagesh2017@gmail.com>
2023-08-14 13:04:32 +0530

    Navigation Interface

--- src/main/java/Project/Building_a_selenium_project/MavenProject.java
+++ src/main/java/Project/Building_a_selenium_project/MavenProject.java
@@ -3,7 +3,6 @@ package Project.Building_a_selenium_project;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
-import org.apache.commons.io.FileUtils;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -11,24 +10,36 @@ import org.openqa.selenium.WebDriver;
 
 //import static org.testng.Assert.assertEquals;
 
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.google.common.io.Files;
 
-public class BrowserStack_MavenProject {
+public class MavenProject {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		// setting the path of the gecko driver executable
+		// ** setting the path of the gecko driver executable
 		System.setProperty("webdriver.gecko.driver",
 				"C:\\Users\\asus\\eclipse\\Selenium_project\\Driver\\geckodriver.exe");
-		// Launch the firefox browser
+		// ** Launch the firefox browser
 		WebDriver driver = new FirefoxDriver();
+		// setting the path of the chrome driver executable
 		// System.setProperty("webdriver.chrome.driver",
-		// "C:\\Users\\asus\\eclipse-Rutuja_java\\com\\target\\Driver\\chromedriver_1.exe");
+              // "C:\\Users\\asus\\eclipse-Rutuja_java\\com\\target\\Driver\\chromedriver_1.exe");
+		// ** Launch the Chrome browser 
 		// WebDriver driver = new ChromeDriver();
-		// Enter the url
+		
+		// ** Enter the url
 		driver.get("https://github.com/mozilla/geckodriver/releases");
+		Thread.sleep(10000);
+		// *********** Navigation Interface ***************
+		
+		driver.navigate().to("http://www.gmail.com");
+		Thread.sleep(2000);
+		driver.navigate().back();
+		Thread.sleep(2000);
+		driver.navigate().forward();
+		Thread.sleep(200);
+		driver.navigate().refresh();
 		// Get the title of the google page and print it on the console
 		String title = driver.getTitle();
 		System.out.println("The title of the page is :" + title);
@@ -38,6 +49,9 @@ public class BrowserStack_MavenProject {
 		// Get the source code of the google page and print it on the console
 		// String pageSource = driver.getPageSource();
 		// System.out.println("the source code of the page is :" + pageSource);
+		
+		//************ TakeScreenshot Interface ************
+		
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File srcFile = ts.getScreenshotAs(OutputType.FILE);
 		Date d = new Date();
@@ -46,6 +60,7 @@ public class BrowserStack_MavenProject {
         System.out.println(date2);
 		File destFile = new File("C:\\Users\\asus\\eclipse\\Selenium_project\\screenshot\\"+date2+"__page.png");
 		Files.copy(srcFile,destFile);
+		
 		// Halt the program execution for 2 seconds
 		Thread.sleep(5000);
 		// Close the browser
